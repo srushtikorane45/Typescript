798e9e36682050a00df13985b5cd7b08c8eb9179
srushtikorane45 <koranesrushti4@gmail.com>
2022-11-20 20:38:01 +0530

    Day1 Introduction
    Day1 Introduction

---
+++ variable.js
@@ -0,0 +1,13 @@
+var n1 = 10;
+console.log(n1);
+var a1 = {
+    productID: 1,
+    ProductName: 'iPhone',
+    productPrice: 25000
+};
+console.log(a1);
+// homodenous array 
+var arr1 = ['Angular', 'React', 'NodeJS', 'java'];
+// heterogenous array 
+var arr2 = ['Angular', 123456, true];
+console.log(arr2);
---
+++ variable.ts
@@ -0,0 +1,19 @@
+var n1: number=10;
+console.log(n1);
+
+var a1: any = {
+    productID: 1,
+    ProductName: 'iPhone',
+    productPrice: 25000
+}
+
+console.log(a1);
+
+// homogenous array 
+var arr1 : Array<string> = ['Angular', 'React', 'NodeJS', 'java'];
+
+// heterogenous array 
+var arr2 : Array<any> = ['Angular', 123456, true];
+
+
+console.log(arr2);
