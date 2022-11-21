b003bc4eac8b0f89792ca2fb680c84faf453f821
srushtikorane45 <koranesrushti4@gmail.com>
2022-11-21 21:34:01 +0530

    calculator
    calculator

---
+++ caluser.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var calc_1 = require("./calc");
+var objCalc = new calc_1.Calculator();
+console.log(objCalc.add(2, 3));
+console.log(objCalc.sub(10, 5));
---
+++ caluser.ts
@@ -0,0 +1,5 @@
+import { Calculator} from './calc';
+
+var objCalc = new Calculator();
+console.log(objCalc.add(2,3));
+console.log(objCalc.sub(10, 5));
