c4fadf096ad60b036be4a9291c264707230b0002
srushtikorane45 <koranesrushti4@gmail.com>
2022-11-21 21:38:32 +0530

    Classes
    Classes

---
+++ classes.js
@@ -0,0 +1,12 @@
+var Flight = /** @class */ (function () {
+    function Flight(flightNo, from, to) {
+        this.flightNo = flightNo;
+        this.from = from;
+        this.to = to;
+    }
+    Flight.prototype.display = function () {
+        console.log(this.flightNo + " " + this.from + " " + this.to);
+    };
+    return Flight;
+}());
+var objFlight = new Flight(123, "Mumbai", "UK");
---
+++ classes.ts
@@ -0,0 +1,25 @@
+interface IFlight {
+    flightNo: number;
+    from: string;
+    to: string;
+}
+
+class Flight implements IFlight {
+    flightNo: number;
+    from: string;
+    to: string;
+
+    constructor(flightNo: number, from: string, to:string) {
+        this.flightNo = flightNo;
+        this.from = from;
+        this.to = to ; 
+    }
+    display() {
+        console.log(this.flightNo + " " + this.from + " "+ this.to);
+    }
+}
+
+var objFlight= new Flight(123,"Mumbai","UK");
+console.log(objFlight);
+objFlight.display()
+
