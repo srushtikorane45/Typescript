6153eff0b24ef8a4a3faddd8313a8d8b18afef60
srushtikorane45 <koranesrushti4@gmail.com>
2022-11-21 21:37:38 +0530

    String
    String

---
+++ stringTypes.js
@@ -0,0 +1,6 @@
+var username = 'CodemindTechnology';
+var s1 = "My name is ".concat(username);
+console.log(username);
+console.log(s1.indexOf('n')); // output 
+console.log(s1.lastIndexOf('n')); // output 
+console.log(s1.charAt((0))); //
---
+++ stringTypes.ts
@@ -0,0 +1,7 @@
+var username : string = 'CodemindTechnology';
+var s1 = `My name is ${username}`
+console.log(username);
+console.log(s1.indexOf('n')) // output 
+console.log(s1.lastIndexOf('n'))// output 
+
+console.log(s1.charAt((0))) ; //
