da051d7dcb1975800fc3423050f1c684a8a20085
srushtikorane45 <koranesrushti4@gmail.com>
2022-11-21 21:31:30 +0530

    Popups
    Popups

---
+++ popup.js
@@ -0,0 +1,5 @@
+console.log('Hey');
+alert('Hey sexy baby');
+confirm("Do you wanna kiss me?");
+var data = prompt("How do you feel?");
+console.log(data);
---
+++ popup.ts
@@ -0,0 +1,6 @@
+console.log('Hello');
+alert('Hello');
+confirm("Do you really want to do this?");
+var data = prompt('Please enter your name');
+
+console.log(data);
